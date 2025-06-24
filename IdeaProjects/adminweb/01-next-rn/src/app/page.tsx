fb5cdc1d0786fdcde9e027fd3ab880fba265efef
trungkien5s <kienkk789gh@gmail.com>
2025-06-24 09:53:08 +0700

    init project

---
+++ IdeaProjects/adminweb/01-next-rn/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+    <div>Hello world with Kien
+      <div>
+        <Button type= "primary">Add new</Button>
+      </div>
+    </div>
+    
+  
+  );
+}
